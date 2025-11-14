06ae0cc12d81d55c26cfdf722170200996825bb5
Hiya Sarkar <sarkarhiya53@gmail.com>
2025-11-14 17:52:26 +0530

    made the aboutsection and calender responsive

--- src/components/Calender.jsx
+++ src/components/Calender.jsx
@@ -3,14 +3,15 @@ import React from "react";
 const Calender = () => {
   return (
     <div>
-      <h2 className="text-[#222222] text-2xl sm:text-3xl md:text-4xl text-center font-serif mt-10 sm:mt-16 drop-shadow-[0_0_10px_rgba(255,0,0,0.8)] [text-shadow:1px_1px_0_red,-1px_-1px_0_red,1px_-1px_0_red,-1px_1px_0_red]">
+      {/* <h2 className="text-[#222222] text-2xl sm:text-3xl md:text-4xl text-center font-serif mt-10 sm:mt-16 drop-shadow-[0_0_10px_rgba(255,0,0,0.8)] [text-shadow:1px_1px_0_red,-1px_-1px_0_red,1px_-1px_0_red,-1px_1px_0_red]">
         Mark Your Calender
       </h2>
       <div
-        className="after:content-[''] after:block after:h-[1.5px] after:w-80 after:bg-red-600 after:mt-1 before:content-[''] before:block before:h-[1.9px] before:w-80 before:bg-red-600 mx-4 sm:mx-8 md:mx-20 lg:mx-40"
-      ></div>    
+        className="after:content-[''] after:block after:h-[1.5px] after:w-full after:bg-red-600 after:mt-1 before:content-[''] before:block before:h-[1.5px] before:w-full before:bg-red-600 mx-4 sm:mx-8 md:mx-20 lg:mx-40"
+      ></div>     */}
+      <img src ="./src/assets/Calender.png" alt = "Calender" className=" mx-auto w-150"/>
       <div className="flex items-center justify-center mt-6 md:mt-8 text-white overflow-x-auto px-4">
-        <div className="flex flex-nowrap items-center justify-center gap-3 md:space-x-8 lg:space-x-10">
+        <div className="flex flex-nowrap items-center justify-center gap-7 md:space-x-8 lg:space-x-10">
           <Timebox value="63" label="Days" />
           <div className="text-red-500 text-xl md:text-5xl font-bold flex flex-col justify-center -mt-2 md:-mt-8">
             :
